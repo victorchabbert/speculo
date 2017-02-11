333fe8bfcfb1497f47f12eaf4439e19817ebaddf
Victor Chabbert <chabbertvi@eisti.eu>
2017-02-11 17:00:33 +0100

    Bootstrap backend

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8080
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => console.log('Listening on port %s', PORT))
