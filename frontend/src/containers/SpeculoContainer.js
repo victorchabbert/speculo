faae59c434a764a0278d18143b9daa3188c8d61f
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-08 14:45:49 +0100

    WIP: move plugin loader to redux

--- frontend/src/containers/SpeculoContainer.js
+++ frontend/src/containers/SpeculoContainer.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import loadPlugins from '../utils/pluginLoader'
 
 import Speculo from '../components/Speculo'
-import Plugin from '../components/PluginPlacement'
 
 
-export default class SpeculoContainer extends Component {
-    render() {
-      const pluginNames = Array(2).fill("weather")
-      const pluginCount = pluginNames.length
-      const plugins = loadPlugins(pluginNames).map((p, i) => <Plugin key={i} count={pluginCount} component={p.component} />)
+class SpeculoContainer extends Component {
 
-      return <Speculo plugins={plugins} />
-    }
+  render() {
+    // const pluginNames = Array(2).fill("weather")
+    // const pluginCount = pluginNames.length
+    // const plugins = loadPlugins(pluginNames)
+    // const pluginDisplay = plugins.map((p, i) => <Plugin key={i} count={pluginCount} component={p.component} />)
+    return <Speculo />
+  }
 }
+
+const mapStateToProps = (state) => ({
+  plugins: []
+})
+
+import { actions as systemActions } from '../redux/system'
+
+const mapDispatchToProps = {
+  loadPlugins: systemActions.loadPlugins
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SpeculoContainer)
--- frontend/src/plugins/weather/index.js
+++ frontend/src/plugins/weather/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import Component from './component'
 
 module.exports = {
+  name: "Weather",
   component: <Component />
 }
