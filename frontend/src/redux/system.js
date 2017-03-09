deb382a8d076c877e8e3295857265c12804e9d41
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-09 11:18:40 +0100

    Add display plugin actions

--- frontend/src/redux/system.js
+++ frontend/src/redux/system.js
@@ -4,17 +4,22 @@ import pluginsLoader from '../utils/pluginLoader'
 
 export const types = {
   SET_ID: 'SET_ID',
-  LOAD_PLUGINS: 'LOAD_PLUGINS'
+  LOAD_PLUGINS: 'LOAD_PLUGINS',
+  DISPLAY_PLUGIN: 'DISPLAY_PLUGIN',
+  HIDE_PLUGIN: 'HIDE_PLUGIN',
 }
 
 export const actions = {
   setID: (id) => ({type: types.SET_ID, id}),
-  loadPlugins: (plugins) => ({type: types.LOAD_PLUGINS, plugins})
+  loadPlugins: (plugins) => ({type: types.LOAD_PLUGINS, plugins}),
+  displayPlugin: (name) => ({type: types.DISPLAY_PLUGIN, name}),
+  hidePlugin: (name) => ({type: types.DISPLAY_PLUGIN, name}),
 }
 
 const initialState = fromJS({
   id: null,
-  plugins: {}
+  plugins: {},
+  active: [],
 })
 
 export default (state = initialState, action) => {
@@ -23,6 +28,21 @@ export default (state = initialState, action) => {
       return state.set('id', action.id)
     case types.LOAD_PLUGINS:
       return state.setIn(['plugins'], state.get('plugins').mergeDeep(action.plugins))
+    case types.DISPLAY_PLUGIN:
+      let newState = state.setIn(['active'], state.get('active').unshift(action.name))
+      if (newState.get('active').size <= 6) {
+        return newState
+      }
+      return newState.setIn(['active'], newState.get('active').pop())
+    case types.HIDE_PLUGIN:
+      if (!state.get('active').has(action.name)) {
+        return state
+      }
+      const index = state.get('active').findIndex(v => v === action.name)
+      if (index === -1) {
+        return state
+      }
+      return state.setIn(['active'], state.get('active').delete(index))
     default:
       return state
   }
@@ -30,6 +50,7 @@ export default (state = initialState, action) => {
 
 // Selectors
 export const getID = state => state.get('id', "Not set")
+export const getActivePlugins = state => state.get('plugins').filter((v, k) => state.get('active').has(k))
 
 // Sagas
 const normalizePlugins = plugins => plugins.reduce((prev, plugin) => {
