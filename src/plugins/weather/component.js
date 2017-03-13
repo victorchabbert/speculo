efc0e4602cb1c0d2e31fd48cc7f13a0fda16d401
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-13 09:45:49 +0100

    Remove frontend plugins

--- src/plugins/weather/component.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import foggySun from './icons/1.svg'
-
-export default (props) => <img src={foggySun} alt="foggy sun" />
--- src/plugins/weather/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Component from './component'
-
-module.exports = {
-  name: "Weather",
-  component: <Component />
-}
