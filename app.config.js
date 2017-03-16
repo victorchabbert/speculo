717b6ab01d52ac118762f604173066dab1c91741
Victor Chabbert <chabbertvi@eisti.eu>
2017-03-16 17:41:04 +0100

    Add dynamic host

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 const PROD = process.env.NODE_ENV === "production"
 module.exports = {
-  host: "localhost",
+  host: PROD ? process.env.IP : "localhost",
   port: PROD ? 80 : 8080,
   plugins: [
     "time",
